64b9562cc7cb84f2f71aca3ef480335c063dd791
fanggq <qw92578@gmail.com>
2019-09-09 16:17:47 +0800

    update config

---
+++ src/main/java/comg/qgf/springcloud/controller/TestController.java
@@ -0,0 +1,22 @@
+package comg.qgf.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Package: comg.qgf.springcloud.controller
+ * @ClassName: TestController
+ * @Author: fgq
+ * @Description:
+ * @Date: 2019/9/6 15:20
+ */
+@RestController
+@RequestMapping("index")
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "hello world";
+    }
+}
