328da7f04d360e7e6e9157ac03b9aef3c59a6491
fanggq <qw92578@gmail.com>
2019-09-10 08:42:19 +0800

    test

---
+++ src/main/java/comg/qgf/springcloud/tes/TestSwitch.java
@@ -0,0 +1,28 @@
+package comg.qgf.springcloud.tes;
+
+/**
+ * @Package: comg.qgf.springcloud.tes
+ * @ClassName: TestSwitch
+ * @Author: fgq
+ * @Description:
+ * @Date: 2019/9/9 14:24
+ */
+public class TestSwitch {
+
+    /**
+     * 测试switch 的String
+     * @param args
+     */
+    public static void main(String[] args) {
+        String s = "aaa";
+
+        switch (s) {
+            case "aaa":
+                System.err.println("dsjadghsajd");
+                break;
+            default:
+                System.err.println("hello world");
+        }
+    }
+
+}
