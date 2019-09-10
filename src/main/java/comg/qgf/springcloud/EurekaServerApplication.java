6b66e0e2d23caf8400a17a733c5bc14b359c52fe
fanggq <qw92578@gmail.com>
2019-09-10 15:03:22 +0800

    项目结构调整

--- src/main/java/comg/qgf/springcloud/EurekaServerApplication.java
+++ src/main/java/comg/qgf/springcloud/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringcloudApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
